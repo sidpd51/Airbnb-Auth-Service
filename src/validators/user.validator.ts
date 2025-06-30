6d14847d574d6a539e8af6b65a480aa170f266bd
sidpd51 <sidpd510@gmail.com>
2025-06-30 19:40:29 +0530

    Add user registration validation schema using Zod

---
+++ src/validators/user.validator.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const userRegisterSchema = z.object({
+    name: z.string({ message: "name is required!" }),
+    email: z.string({ required_error: "Email is required" }).email({ message: "Invalid email address" }),
+    password: z.string({
+        required_error: "Password is required",
+    }).regex(strongPasswordRegex, "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
+});
